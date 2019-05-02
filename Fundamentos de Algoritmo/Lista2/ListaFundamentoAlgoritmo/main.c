fa38b9b7f9ecfca264e052629d5cdbefc0d158eb
Thiago Boccalon <thiago_rodrigo90@hotmail.com>
2019-05-02 20:44:22 -0300

    exercicio_9_and_10

--- Fundamentos de Algoritmo/Lista2/ListaFundamentoAlgoritmo/main.c
+++ Fundamentos de Algoritmo/Lista2/ListaFundamentoAlgoritmo/main.c
@@ -3,6 +3,57 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    //exercicio_09();
+    exercicio_10();
     return 0;
 }
+
+
+void exercicio_09(){
+    double valor = 0;
+
+    printf("Escreva um valor: ");
+    scanf("%d", &valor);
+
+    if(valor == 0){
+        printf("O valor digitado eh igual a Zero.");
+    }
+    else{
+        printf("O valor digitado eh diferente de Zero.");
+    }
+}
+
+
+void exercicio_10(){
+    double VALOR_MACAS_ATE_UMA_DUZIA = 1.3;
+    double VALOR_MACAS_ACIMA_DE_UMA_DUZIA  = 1;
+
+    int quantidade_macas_compradas = 0;
+    double valor_total_das_macas = 0;
+
+
+    printf("Informe a quantidade de macas compradas: ");
+    scanf("%d", &quantidade_macas_compradas);
+
+    printf(quantidade_macas_compradas);
+
+    if(quantidade_macas_compradas > 0){
+        if(quantidade_macas_compradas < 12){
+            valor_total_das_macas = quantidade_macas_compradas * VALOR_MACAS_ATE_UMA_DUZIA;
+        }
+        else{
+            valor_total_das_macas = quantidade_macas_compradas * VALOR_MACAS_ACIMA_DE_UMA_DUZIA;
+        }
+
+        printf("Valor total: %d", valor_total_das_macas);
+    }
+    else
+    {
+        printf("Error... valor invalido.");
+    }
+
+
+}
+
+
+
